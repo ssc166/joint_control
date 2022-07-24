da48d8cc1f1a5ab514cc320698d46a085e638c37
seungchul <ssc166@naver.com>
2022-07-24 19:41:08 +0900

    WIP joint control

--- src/Cal_CoM.py
+++ src/Cal_CoM.py
@@ -7,6 +7,7 @@ from std_msgs.msg import Float64MultiArray, Float64
 from gazebo_msgs.srv import GetModelState, ApplyBodyWrench, GetLinkState
 import math
 import WIP_utils as utils
+import modern_robotics as mr
 
 def get_link_state(link_name_main, reference_frame):
     
@@ -17,7 +18,6 @@ def get_link_state(link_name_main, reference_frame):
     return link_state
 
 def get_link_ori(link_name_main, reference_frame):
-    global link_state
     
     link_state = get_link_state(link_name_main, reference_frame)
     
@@ -28,7 +28,18 @@ def get_link_ori(link_name_main, reference_frame):
      
     return link_ori_x, link_ori_y, link_ori_z
 
+def get_link_vel(link_name_main, reference_frame):
+    
+    link_state = get_link_state(link_name_main, reference_frame)
+    
+    link_vel_x = link_state.link_state.twist.angular.x
+    link_vel_y = link_state.link_state.twist.angular.y
+    link_vel_z = link_state.link_state.twist.angular.z
+    
+    return link_vel_x, link_vel_y, link_vel_z
+
 def get_cur_deg():
+    global link_name_list
 
     ankle_link = 'ankle_link'
     knee_ankle_link = 'knee_ankle_link'
@@ -42,10 +53,22 @@ def get_cur_deg():
     L4_x, L4_y, L4_z = get_link_ori(link_name_list[3], link_name_list[2])
 
     thetalist = np.array([L1_y, L2_y, L3_y, L4_y])
-
+ 
     return thetalist
 
-def Cal_Traj():
+def get_cur_vel():
+
+    L1_x, L1_y, L1_z = get_link_vel(link_name_list[0])
+    L2_x, L2_y, L2_z = get_link_vel(link_name_list[1])
+    L3_x, L3_y, L3_z = get_link_vel(link_name_list[2])
+    L4_x, L4_y, L4_z = get_link_vel(link_name_list[3])
+
+    dthetalist = np.array([L1_y, L2_y, L3_y, L4_y])
+ 
+    return dthetalist    
+
+def Cal_Deg_Traj():
+    global time, traj
 
     thetalist = get_cur_deg()
 
@@ -55,12 +78,79 @@ def Cal_Traj():
     traj_th3 = utils.Path_Gen(thetalist[2], theta[2], traj[:,0])
     traj_th4 = utils.Path_Gen(thetalist[3], theta[3], traj[:,0])
 
-    n = len(time)
+    return traj_th1, traj_th2, traj_th3, traj_th4
 
-    return n, traj_th1, traj_th2, traj_th3, traj_th4
+def Cal_Vel_Traj():
+
+    thetalist = get_cur_deg()
+
+    traj_dth1 = utils.Path_Gen(thetalist[0], theta[0], traj[:,1])
+    traj_dth2 = utils.Path_Gen(thetalist[1], theta[1], traj[:,1])
+    traj_dth3 = utils.Path_Gen(thetalist[2], theta[2], traj[:,1])
+    traj_dth4 = utils.Path_Gen(thetalist[3], theta[3], traj[:,1])
+
+    return traj_dth1, traj_dth2, traj_dth3, traj_dth4
+
+def Cal_Acc_Traj():
+
+    thetalist = get_cur_deg()
+
+    traj_ddth1 = utils.Path_Gen(thetalist[0], theta[0], traj[:,2])
+    traj_ddth2 = utils.Path_Gen(thetalist[1], theta[1], traj[:,2])
+    traj_ddth3 = utils.Path_Gen(thetalist[2], theta[2], traj[:,2])
+    traj_ddth4 = utils.Path_Gen(thetalist[3], theta[3], traj[:,2])
+
+    return traj_ddth1, traj_ddth2, traj_ddth3, traj_ddth4
+
+def get_torque_traj():
+
+    thetalist = get_cur_deg()
+    dthetalist = get_cur_vel()
+
+    eint = np.array([0.2, 0.2, 0.2])
+    g = np.array([0, 0, -9.8])
+    M01 = np.array([[1, 0, 0,        0],
+                    [0, 1, 0,        0],
+                    [0, 0, 1, 0.089159],
+                    [0, 0, 0,        1]])
+    M12 = np.array([[ 0, 0, 1,    0.28],
+                    [ 0, 1, 0, 0.13585],
+                    [-1, 0, 0,       0],
+                    [ 0, 0, 0,       1]])
+    M23 = np.array([[1, 0, 0,       0],
+                    [0, 1, 0, -0.1197],
+                    [0, 0, 1,   0.395],
+                    [0, 0, 0,       1]])
+    M34 = np.array([[1, 0, 0,       0],
+                    [0, 1, 0,       0],
+                    [0, 0, 1, 0.14225],
+                    [0, 0, 0,       1]])
+    G1 = np.diag([0.010267, 0.010267, 0.00666, 3.7, 3.7, 3.7])
+    G2 = np.diag([0.22689, 0.22689, 0.0151074, 8.393, 8.393, 8.393])
+    G3 = np.diag([0.0494433, 0.0494433, 0.004095, 2.275, 2.275, 2.275])
+    Glist = np.array([G1, G2, G3])
+    Mlist = np.array([M01, M12, M23, M34])
+    Slist = np.array([[1, 0, 1,      0, 1,     0],
+                        [0, 1, 0, -0.089, 0,     0],
+                        [0, 1, 0, -0.089, 0, 0.425]]).T
+    thetalistd = np.array([theta[0], theta[1], theta[2], theta[3]])
+    dthetalistd = np.array([2, 1.2, 2])
+    ddthetalistd = np.array([0.1, 0.1, 0.1])
+    Kp = 1.3
+    Ki = 1.2
+    Kd = 1.1
+
+    mr.ComputedTorque(thetalist, dthetalist, eint, g, Mlist, Glist, Slist, \
+                   thetalistd, dthetalistd, ddthetalistd, Kp, Ki, Kd)
+
+
+
 
 def callback(data):
+
     global theta
+    global L1, L2, L3, L4
+
     theta = data.data
     theta_1 = theta[0]
     theta_2 = theta[0] + theta[1]
@@ -109,7 +199,7 @@ def callback(data):
 if __name__ == '__main__':
     try:  
         while True:
-            rospy.init_node('listener', anonymous=True)
+            rospy.init_node('CoM_Calculator', anonymous=True)
             rospy.Subscriber('array', Float64MultiArray,callback)
             pub_roll = rospy.Publisher('CoM_Roll', Float64MultiArray, queue_size=100)
             pub_pitch = rospy.Publisher('CoM_Pitch', Float64MultiArray, queue_size=100)
